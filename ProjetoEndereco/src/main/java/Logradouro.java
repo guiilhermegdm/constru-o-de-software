7b07ad4dc2b88fc0f003cb80c4d40d1e66143452
guiilhermegdm <guilhermedutra@discente.ufg.br>
2024-04-19 16:39:23 -0300

    Adicionando classes Logradouro e Pais

---
+++ ProjetoEndereco/src/main/java/Logradouro.java
@@ -0,0 +1,5 @@
+public class Logradouro {
+    private TipoLogradouro tipoLogradouro;
+    private String nome;
+    private String complemento;
+}
---
+++ ProjetoEndereco/src/main/java/Pais.java
@@ -0,0 +1,5 @@
+public class Pais {
+    private String nome;
+
+    //Getters and setters
+}
