783510633ed18412e7251f4032ff7faec6b1e5dc
guiilhermegdm <guilhermedutra@discente.ufg.br>
2024-04-19 16:41:17 -0300

    Adicionando ultimas classes

---
+++ ProjetoEndereco/src/main/java/PessoaFisica.java
@@ -0,0 +1,9 @@
+public class PessoaFisica {
+    private String nome;
+    private String CPF;
+
+    private String sexo;
+    private Date dataNascimento;
+
+  //Getters and setters
+}
---
+++ ProjetoEndereco/src/main/java/TipoEndereco.java
@@ -0,0 +1,5 @@
+public enum TipoEndereco {
+    RESIDENCIAL,
+    COMERCIAL,
+    OUTRO;
+}
