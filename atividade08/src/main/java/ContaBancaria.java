127a6f1d269a3aaaf8c0dd92341570749e798134
guiilhermegdm <guilhermedutra@discente.ufg.br>
2024-04-30 12:16:11 -0300

    Realizando ex 4 conta bancaria

---
+++ atividade08/src/main/java/ContaBancaria.java
@@ -0,0 +1,25 @@
+public class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+    }
+
+    public void sacar(double valor) throws SaldoInsuficienteException {
+        if (valor > saldo) {
+            throw new SaldoInsuficienteException("Saldo insuficiente para realizar o saque.");
+        } else {
+            saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
